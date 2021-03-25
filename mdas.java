7df3ed54e1a6e4bbd81f69e20c505007be08959f
Jl-deleon <79123040+Jl-deleon@users.noreply.github.com>
2021-03-25 14:53:45 +0800

    Add files via upload

---
+++ mdas.java
@@ -0,0 +1,36 @@
+public class mdas{
+	private int num1;
+	private int num2;
+	
+	public void setnum1 (int num){
+	num1=num;
+	}
+	
+	public int getnum1(){
+	return num1;
+	}
+	
+	public void setnum2 (int num){
+	num2=num;
+	}
+	public int getnum2(){
+	return num2;
+	}
+
+	public int diff(int n1,int n2){
+	return n1-n2;
+	}
+
+	public int add(int n1,int n2){
+		return n1+n2;
+		}
+	public int mult(int n1,int n2){
+		return n1*n2;
+		}
+
+	public double qout(int n1,int n2){
+		return (double)n1/n2;
+		}
+	
+	}
+
